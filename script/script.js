c728ad2bc0989e5c30370b27478167f9e7f4fdd8
Wendell Delmondes <wendell2009@hotmail.com>
2023-06-26 22:49:04 -0300

    projeto enviado nota 5.9

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
-var dadosTeste={
-    "codigoProduto": 111,
-    "descricaoProduto": "Produto 1 teste",
+/*var dadosTesteSemApi={  //****VARIÁVEL PARA TESTE RÁPIDO DE LAYOUT, COM PADRÃO DE RECEBIMENTO VIA API
+    "codigoProduto": 1,     //***SETA A VARIAVEL (dados) na linha 146 deste aquivo, NECESSITA RECARREGAR PAGINA...
+    "descricaoProduto": "Produto 1", // POIS O LOOP DE LAYOUT DEPENDE DA RESPOSTA DA API
     "taxaJuros": 0.0179,
     "resultadoSimulacao": 
     [
@@ -75,52 +75,88 @@ var dadosTeste={
     ]
     }
     ]
+    }*/
+
+    document.querySelector("#valor").addEventListener('input',function(){// layout amigável para frontEnd
+                                                                        //processa a cada digito novo no input #valor(event input)
+    this.value=this.value.replace(/[^\d]/g,"");//excui tudo que Não é dígito(0-9) da entrada
+    this.value=this.value.replace(/^[0]+/g,"");//exclui zeros a esquerda
+    switch (this.value.length){
+        case 0:break;
+        case 1:
+            this.value="0,0"+this.value;
+            break;
+        case 2:
+            this.value="0,"+this.value;
+            break; 
+        case 3:case 4:case 5:
+            this.value=this.value.replace(/(\d{1,3})(\d{2})/g, '$1,$2');
+            break;    
+        case 6:case 7:case 8:
+            this.value=this.value.replace(/(\d{1,3})(\d{3})(\d{2})/g, '$1.$2,$3');
+            break;  
+        case 9:case 10:case 11:
+            this.value=this.value.replace(/(\d{1,3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3,$4');
+             break;  
+        case 12:case 13:case 14:
+            this.value=this.value.replace(/(\d{1,3})(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3.$4,$5');
+            break;
+        case 15:case 16:case 17:
+            this.value=this.value.replace(/(\d{1,3})(\d{3})(\d{3})(\d{3})(\d{3})(\d{2})/g,'$1.$2.$3.$4.$5,$6');
+            break;    
+        default:        
+            alert("Valor Acima dos parâmetros estabelecidos para o produto");
+            this.value=this.value.replace(/[\d]$/,"");
+            this.value=this.value.replace(/(\d{1,3})(\d{3})(\d{3})(\d{3})(\d{3})(\d{2})/g,'$1.$2.$3.$4.$5,$6');
     }
-
-document.getElementById('apiForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    var valorDesejado = document.getElementById('valor').value;
-    var prazo = document.getElementById('prazo').value;
-
-    var requestData = {
-        valorDesejado: parseInt(valorDesejado),
-        prazo: parseInt(prazo)
-    };
-    enviaDadosApi(requestData);
-   
 });
 
+    document.getElementById('apiForm').addEventListener('submit', function(event) {
+    event.preventDefault();//aborta comportamento de envio padão do form
 
+    var valorDesejado = document.getElementById('valor').value;//espera somente digitos, filto na linha 80 a 111
+    var prazo = document.getElementById('prazo').value; //validação no próprio input(#prazo) no arquivo index.html 
+    valorDesejado=parseFloat(valorDesejado.replace(/[^\d]/g,"")/100);//conversão em number com 2 casas decimais após filtrar somente os digitos(númericos)
+      
+    var requestData = {
+        valorDesejado: valorDesejado,
+        prazo: parseInt(prazo) //prazo validado no próprio formulário html
+    };
+    if(!isNaN( valorDesejado)){ //teste de validade
+     enviaDadosApi(requestData);
+     
+    }else {
+        alert("O Valor Desejado digitado é INVÁLIDO, tente novamente");
+    }
+});
 
 function enviaDadosApi(requestData){
    
     
-
     fetch( "https://apphackaixades.azurewebsites.net/api/Simulacao",{
         method:"POST",
         headers:{
             "Content-Type":"application/json",
         },
         body : JSON.stringify(requestData),
-    }).then((response)=>response.json())
-    .then((dados)=>{
-      
-        console.log("API não respondendo como esperado! Usando dados Teste");
+    }).then((response)=>{
+        response.json();
+       
+    }).then((dados)=>{
+    //  dados=dadosTesteSemApi;//dados para teste sem concexão com api
+        if(dados!=undefined) modelaResposta(dados);
         
-         modelaResposta(dados);
     }).catch((err=>{
-        console.log("no no recebimento de dados  com fetch");
+        console.log("erro no recebimento de dados  com fetch");
         
     }));
 }
 
 function modelaResposta(dados){
-     document.getElementById("simulacoes").classList.remove("d-none");//classList.add("")
-
-    dados=dadosTeste;
-   
-    document.getElementById("descricaoProduto").innerHTML=dados.descricaoProduto;
+    document.querySelector("#alertaSucesso").classList.add("show");
+    document.querySelector("#novaSimulacao").classList.remove("show");
+     document.getElementById("simulacoes").classList.remove("d-none");
+     document.getElementById("descricaoProduto").innerHTML=dados.descricaoProduto;
     document.getElementById("codigoProduto").innerHTML=dados.codigoProduto;
     document.getElementById("taxaJuros").innerHTML=dados.taxaJuros;
     
@@ -130,15 +166,15 @@ function modelaResposta(dados){
              tabela=document.getElementById("tabelaSAC");
              renovaTabela(tabela);
              
-            alert("entrou SAC")
+            
         }else if(dados.resultadoSimulacao[i].tipo=="PRICE"){
-            alert("entrou price")
+           
             tabela=document.getElementById("tabelaPRICE"); 
             renovaTabela(tabela);
         }
        let numeroParcelas=dados.resultadoSimulacao[i].parcelas.length;   
-       alert("parcelas:"+numeroParcelas);
-       console.log(tabela);
+       
+     //  console.log(tabela);
        for(let j=0;j<numeroParcelas;j++){
             const tr=document.createElement("tr");
             const td1=document.createElement("td");
@@ -158,7 +194,7 @@ function modelaResposta(dados){
             
             var tabelaCorpo=tabela.lastElementChild;
             tabelaCorpo.appendChild(tr);
-            console.log(tabelaCorpo);
+          //  console.log(tabelaCorpo);
 
         }
     }
@@ -184,9 +220,15 @@ function criaLinhaTabela(dados){
         tr.appendChild(td4);
 
         const table=document.getElementById("tableSAC");
-        console.log(table);
         const tabelaCorpo=table.lastElementChild;
         tabelaCorpo.appendChild(tr);
-        console.log(tabelaCorpo);
+      
     
+}
+function fechaAlertaSucesso(){
+    if( document.querySelector('#alertaSucesso')!=null){
+        document.querySelector('#alertaSucesso').classList.remove("show");
+    }
+   document.getElementById("simulacoes").classList.add("d-none");
+   
 }
